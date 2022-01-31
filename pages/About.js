2a0352fe690697dbac0b48bd9635b883026dcfd2
Samuel Panek <samuelrpanek@gmail.com>
2022-01-31 15:14:27 -0800

    adds about page

---
+++ pages/About.js
@@ -0,0 +1,11 @@
+export default function About(){
+  return(
+    <div>
+      <h3>Members of the Team:</h3>
+      <p> Shan Jiang</p>
+      <p> K`&apos;`Lan Stovall</p>
+      <p> Chris Reichert</p>
+      <p> Sam Panek</p>
+    </div>
+  )
+}
