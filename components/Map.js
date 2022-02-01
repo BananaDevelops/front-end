a061ee682585ea948219b6cb3581dc49f4f50922
Samuel Panek <samuelrpanek@gmail.com>
2022-01-31 17:58:06 -0800

    adds axios call for map info
    Co-authored-by: Chris Reichert<chris.a.reichert@gmail.com>
    Co-authored-by: K'Lan Stoval <kland13@gmail.com>
    Co-authored-by: Shan Jiang <johnson33399@gmail.com>

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,35 @@
 import Image from "next/image"
+import { useState } from "react"
 import myLoader from "./Loader"
+import axios from "axios"
 
 export default function Map({Maproom}) {
+
+  const [BackEndData, setBackEndData] = useState('')
+
+  async function handleButton(event){
+    const url= "https://stick-figure-backend.herokuapp.com/game_logic/test_map/"
+    try{
+      const response = await axios.get(url)
+      console.log(response.data.Data)
+      setBackEndData(response.data.Data)
+    }
+    catch(error){
+      
+      console.log("You lose", error.message)
+
+    }
+  }
+
   return (
     <div className="inline border-4 border-black w-1/4">
       <p className="bg-gray-200 text-lg font-bold">Map: </p>
       <p>You are in Room {Maproom}</p>
       <div className="justify-center"> 
         <MapImage/>
+      </div>
+      <div>
+        {BackEndData ? <p>{BackEndData}</p> : <button onClick={handleButton}>Press Me</button>}
       </div>
     </div>
   )
