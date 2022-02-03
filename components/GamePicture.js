552055dd5ac29148a10d2db57745f9cf8204e0e0
KLan Stovall <kland13@gmail.com>
2022-02-03 14:45:30 -0800

    fixed images on production
    Co-authored-by: Chris Reichert <chris.a.reichert@gmail.com>
    Co-authored-by: Shan Jiang <johnson33399@gmail.com>
    Co-authored-by: Samuel Panek <samuelrpanek@gmail.com>

--- components/GamePicture.js
+++ components/GamePicture.js
@@ -6,8 +6,8 @@ export default function GamePicture({Player}) {
     <div className="w-1/2 border-4 border-green rounded-2xl bg-black">
       <div className="flex my-4 justify-center">
         {/*this need Player.combat in true test */}
-        {Player.combat != false ? <Image src="/../public/static/img/monster.png" alt="Picture of the  Monster" width={400} height={400}
-    /> : <Image src="/../public/static/img/corridor.png" alt="Picture of the  Monster" width={600} height={400}/> }
+        {Player.combat != false ? <Image src="/static/img/monster.png" alt="Picture of the  Monster" width={400} height={400}
+    /> : <Image src="/static/img/corridor.png" alt="Picture of the  Monster" width={600} height={400}/> }
       </div>
     </div>
   )
@@ -17,7 +17,7 @@ export default function GamePicture({Player}) {
 
 const GameImage = (props) => {
   return (
-    <Image
+    <image
       loader={myLoader}
       src="/../public/static/img/monster.png"
       alt="Picture of the  Monster"
